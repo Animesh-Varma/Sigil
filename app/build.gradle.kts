7459d223a02faae5b8730aa4f8a40c48ba344726
animesh_varma <animesh_varma@protonmail.com>
2025-12-10 01:42:50 +0530

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.animeshvarma.sigil"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.0"
+        versionCode = 6
+        versionName = "0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
